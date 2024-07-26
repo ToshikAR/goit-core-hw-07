f1e6bb0709a7ad00f695f79a43d0555298d677f1
R.Anatolii <tv.weplay@gmail.com>
2024-07-26 00:15:24 -0700

    Task

---
+++ main.py
@@ -0,0 +1,85 @@
+'''
+Настав час об'єднати наші попередні домашні завдання в одне.
+Зараз ми нарешті ми об'єднаємо попередні домашні завдання в один функціональний проєкт
+'''
+from bookPackage import AddressBook, function
+
+
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
+
+def get_commands():
+    help = [
+        "close",
+        "exit",
+        "all",
+        "add [ім'я] [телефон]",
+        "change [ім'я] [старий телефон] [новий телефон]",
+        "phone [ім'я]",
+        "add-birthday [ім'я] [дата народження]",
+        "show-birthday [ім'я]",
+        "birthdays"
+    ]
+    text = "Command: \n" 
+    for i in help:
+        text += i + "\n"
+    return text
+
+def main():
+    print("Welcome to the assistant bot!")
+    book = AddressBook()
+
+    while True:
+        user_input = input("Enter a command: ")
+        if user_input.strip() == '': 
+            print('Enter the command')
+            continue
+        
+        command, *args = parse_input(user_input)
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+
+        elif command == "hello":
+            print("How can I help you?")
+
+        elif command == "add":
+            # реалізація
+            print(function.add_contact(args, book))
+
+        elif command == "change":
+            # реалізація
+            print(function.change_contact(args, book))
+
+        elif command == "phone":
+            # реалізація
+            print(function.find_contact(args, book))
+
+        elif command == "all":
+            # реалізація
+            print(book)
+
+        elif command == "add-birthday":
+            # реалізація
+            print(function.add_birthday(args, book))
+
+        elif command == "show-birthday":
+            # реалізація
+            print(function.show_birthday(args, book))
+
+        elif command == "birthdays":
+            # реалізація
+            print(book.get_upcoming_birthdays())
+
+        elif command == "help":
+            # реалізація
+            print(get_commands())
+
+        else:
+            print("Invalid command.")
+
+
+if __name__ == "__main__":
+    main()
